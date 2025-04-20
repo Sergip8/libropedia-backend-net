6ef35076c66ba396fbf583ba5953dbb66ab15e22
sergip8 <sagp758@gmail.com>
2025-04-20 15:30:29 -0500

    fix test

--- Libropedia.Tests/UnitTest1.cs
+++ Libropedia.Tests/UnitTest1.cs
@@ -1,10 +1,10 @@
-﻿namespace Libropedia.Tests;
+﻿// namespace Libropedia.Tests;
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
+// public class UnitTest1
+// {
+//     [Fact]
+//     public void Test1()
+//     {
 
-    }
-}
+//     }
+// }
